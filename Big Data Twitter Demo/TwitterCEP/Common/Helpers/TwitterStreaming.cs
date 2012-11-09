b31f326041707020281f8764a408318ff52d3c6f
Amoll <andrmoll@MININT-3SVTKLD.northamerica.corp.microsoft.com>
2012-11-09 10:31:26 -0600

    Version 0.8

---
+++ Big Data Twitter Demo/TwitterCEP/Common/Helpers/TwitterStreaming.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace StreamInsight.Demos.Twitter.Common
+{
+    public class TwitterStreaming
+    {
+        private TwitterConfig _config;
+
+        public TwitterStreaming(TwitterConfig config)
+        {
+            this._config = config;
+        }
+
+        public TextReader Read()
+        {
+            var url = GetURL();
+            var request = HttpWebRequest.Create(url);
+            request.Timeout = _config.Timeout;
+
+            request.Credentials = new NetworkCredential(_config.Username, _config.Password);
+            var response = request.GetResponse();
+
+            return new StreamReader(response.GetResponseStream());
+        }
+
+        private string GetURL()
+        {
+            string result = this._config.StreamingURL;
+
+            if (!string.IsNullOrEmpty(_config.Parameters))
+                result += "?track=" + _config.Parameters;
+
+            return result;
+        }
+    }
+}
